e825c82914244237e0d749d8cb3dd2079c656730
Tejas Sawant <tejassawant027@gmail.com>
2026-04-18 16:59:41 +0530

    change colourscheme

--- lua/custom/plugins/colour-scheme.lua
+++ lua/custom/plugins/colour-scheme.lua
@@ -1,12 +1,36 @@
 return {
   -- 'Shatur/neovim-ayu',
-  'kepano/flexoki-neovim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'flexoki-light'
-    -- vim.cmd.colorscheme 'ayu-dark'
-    vim.cmd.hi 'Comment gui=none'
+  -- 'kepano/flexoki-neovim',
+  'rebelot/kanagawa.nvim',
+  opts = {
+    compile = false, -- enable compiling the colorscheme
+    undercurl = true, -- enable undercurls
+    commentStyle = { italic = false },
+    functionStyle = {},
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false, -- do not set background color
+    dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true, -- define vim.g.terminal_color_{0,17}
+    -- colors = { -- add/modify theme and palette colors
+    --   palette = {},
+    --   theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    -- },
+    -- overrides = function(colors) -- add/modify highlights
+    --   return {}
+    -- end,
+    theme = 'dragon',
+    background = {
+      dark = 'dragon',
+      light = 'lotus',
+    },
+  },
+  config = function(_, opts)
+    require('kanagawa').setup(opts)
+
+    vim.cmd.colorscheme 'kanagawa'
+    -- vim.cmd.hi 'Comment gui=none'
   end,
 }
 
